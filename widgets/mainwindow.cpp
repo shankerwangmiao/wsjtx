1febce6802ca615031737c154e9d149d56c94495
Uwe Risse <dg2ycb@gmx.de>
2024-10-20 10:16:37 +0200

    Some more changes to the QSYMessage Creator + reformatting the code. Work in progress.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1327,42 +1327,38 @@ void MainWindow::writeSettings()
 
 void MainWindow::update_tx5(const QString &qsy_text)
 {
-	if((ui->tx5->findText(qsy_text)) == -1)
-	{	
-		ui->tx5->addItem(qsy_text);
-		ui->tx5->setCurrentIndex(ui->tx5->count() - 1);
-	}
-	else
-	{
-		ui->tx5->setCurrentIndex(ui->tx5->findText(qsy_text));
-	}	
-	ui->txb5->click();
+  if((ui->tx5->findText(qsy_text)) == -1) {
+    ui->tx5->addItem(qsy_text);
+    ui->tx5->setCurrentIndex(ui->tx5->count() - 1);
+  } else {
+    ui->tx5->setCurrentIndex(ui->tx5->findText(qsy_text));
+  }
+  ui->txb5->click();
+  if(!m_auto) ui->autoButton->click();
+  QTimer::singleShot (1900*m_TRperiod, [=] {if(m_auto)ui->autoButton->click();});
 }
 
 void MainWindow::reply_tx5(const QString &qsy_reply)
 {
-	if((ui->tx5->findText(qsy_reply)) == -1)
-	{	
-		ui->tx5->addItem(qsy_reply);
-		ui->tx5->setCurrentIndex(ui->tx5->count() - 1);
-	}
-	else
-	{
-		ui->tx5->setCurrentIndex(ui->tx5->findText(qsy_reply));
-		if(!m_auto) ui->autoButton->click();
-		QTimer::singleShot (1000*m_TRperiod, [=] {if(m_auto)ui->autoButton->click();});
-	}	
-	ui->txb5->click();
+  if((ui->tx5->findText(qsy_reply)) == -1) {
+    ui->tx5->addItem(qsy_reply);
+    ui->tx5->setCurrentIndex(ui->tx5->count() - 1);
+  } else {
+    ui->tx5->setCurrentIndex(ui->tx5->findText(qsy_reply));
+  }
+  ui->txb5->click();
+  if(!m_auto) ui->autoButton->click();
+  QTimer::singleShot (1900*m_TRperiod, [=] {if(m_auto)ui->autoButton->click();});
 }
 
 void MainWindow::update_QSYMessageCreatorCheckBoxStatus(const bool &chkBoxValue)
 {
-	m_QSYMessageCheckBoxValue = chkBoxValue;
+  m_QSYMessageCheckBoxValue = chkBoxValue;
 }
 
 void MainWindow::setQSYMessageCreatorStatus(const bool &QSYMessageCreatorValue)
 {
-	m_QSYMessageCreatorValue = QSYMessageCreatorValue;
+  m_QSYMessageCreatorValue = QSYMessageCreatorValue;
 }
 
 //---------------------------------------------------------- readSettings()
@@ -2128,96 +2124,79 @@ void MainWindow::showStatusMessage(const QString& statusMsg)
 
 void MainWindow::showQSYMessage(QString message)
 {
-  QString the_line = message;  
-  QString qCall = QString(m_config.my_callsign ()); 
+  QString the_line = message;
+  QString qCall = QString(m_config.my_callsign ());
   QString qDXCall = ui->dxCallEntry->text();
-  if(the_line.contains(qCall + QString(" ")))
-  {	  
-	  QStringList bhList = the_line.split(" ",SkipEmptyParts);
-	  qsizetype index = (bhList.indexOf(qCall)); 
-	  if(index != (-1))
-	  {
-		  QString finalMatch = "";
-		  QRegularExpression re1("[A-J][V48MW][0-9]{3}");
-		  QRegularExpression re2("(92)[V48MW][0-9]{3}");
-		  QRegularExpression re3("(93)[V48MW][0-9]{3}");
-		  
-		  QRegularExpressionMatch match = re1.match(the_line,QRegularExpression::NormalMatch);
-		  if(match.hasMatch())
-		  {
-		       int startOffset = match.capturedStart(); 
-		       int endOffset = match.capturedEnd();
-		       finalMatch = the_line.mid(startOffset,(1+endOffset-startOffset));
-		  }
-		  else
-		  {
-		  	match = re2.match(the_line,QRegularExpression::NormalMatch);
-		  	if(match.hasMatch())
-		  	{
-			    int startOffset = match.capturedStart(); 
-			    int endOffset = match.capturedEnd();
-			    finalMatch = the_line.mid(startOffset,(1+endOffset-startOffset));
-		  	}
-		  	else
-		  	{
-		  		match = re3.match(the_line,QRegularExpression::NormalMatch);
-			  	if(match.hasMatch())
-			  	{
-				    int startOffset = match.capturedStart(); 
-				    int endOffset = match.capturedEnd();
-				    finalMatch = the_line.mid(startOffset,(1+endOffset-startOffset));
-			  	}
-		  	}
-		  	
-		  }	
-		  if(finalMatch.size()>=5)
-		  {
-			  m_QSYMessageWidget.reset (new QSYMessage(finalMatch, qCall)); 
-			  
-			  //connect to signal finish
-			  connect (this, &MainWindow::finished, &QSYMessage::close);
-			  
-			  //connect to signal from QSYMessage
-			  connect (m_QSYMessageWidget.data (), &QSYMessage::sendReply, this, &MainWindow::reply_tx5,Qt::UniqueConnection); 			  
-			  QPoint mainPos = this->mapToGlobal(QPoint(0,0));
-			  m_QSYMessageWidget->show();
-			  m_QSYMessageWidget->move(mainPos.x() + this->width()/2 - (m_QSYMessageWidget->width())/2, mainPos.y() + this->height()/2 - (m_QSYMessageWidget->height())/2);
-			  m_QSYMessageWidget->raise();
-			  m_QSYMessageWidget->activateWindow();	
-		  }
-	  }
+  if(the_line.contains(qCall + QString(" "))) {
+    QStringList bhList = the_line.split(" ",SkipEmptyParts);
+    qsizetype index = (bhList.indexOf(qCall));
+    if(index != (-1)) {
+      QString finalMatch = "";
+      QRegularExpression re1("[A-J][V48MW][0-9]{3}");
+      QRegularExpression re2("(92)[V48MW][0-9]{3}");
+      QRegularExpression re3("(93)[V48MW][0-9]{3}");
+
+      QRegularExpressionMatch match = re1.match(the_line,QRegularExpression::NormalMatch);
+      if(match.hasMatch()) {
+           int startOffset = match.capturedStart();
+           int endOffset = match.capturedEnd();
+           finalMatch = the_line.mid(startOffset,(1+endOffset-startOffset));
+      } else {
+        match = re2.match(the_line,QRegularExpression::NormalMatch);
+        if(match.hasMatch()) {
+          int startOffset = match.capturedStart();
+          int endOffset = match.capturedEnd();
+          finalMatch = the_line.mid(startOffset,(1+endOffset-startOffset));
+        } else {
+          match = re3.match(the_line,QRegularExpression::NormalMatch);
+          if(match.hasMatch()) {
+            int startOffset = match.capturedStart();
+            int endOffset = match.capturedEnd();
+            finalMatch = the_line.mid(startOffset,(1+endOffset-startOffset));
+          }
+        }
+      }
+      if(finalMatch.size()>=5) {
+        m_QSYMessageWidget.reset (new QSYMessage(finalMatch, qCall));
+
+        //connect to signal finish
+        connect (this, &MainWindow::finished, &QSYMessage::close);
+
+        //connect to signal from QSYMessage
+        connect (m_QSYMessageWidget.data (), &QSYMessage::sendReply, this, &MainWindow::reply_tx5,Qt::UniqueConnection);
+        QPoint mainPos = this->mapToGlobal(QPoint(0,0));
+        m_QSYMessageWidget->show();
+        m_QSYMessageWidget->move(mainPos.x() + this->width()/2 - (m_QSYMessageWidget->width())/2, mainPos.y() + this->height()/2 - (m_QSYMessageWidget->height())/2);
+        m_QSYMessageWidget->raise();
+        m_QSYMessageWidget->activateWindow();
+      }
+    }
   }
-  else if(the_line.contains(qDXCall + QString(" ")))
-  {  
-	  if ((the_line.contains("OKQSY") || the_line.contains("NOQSY")) )
-	  { 	  
-		  QStringList bhList = the_line.split(" ",SkipEmptyParts);
-		  qsizetype index = (bhList.indexOf(qDXCall)); 
-		  if(index != (-1))
-		  {	
-		  	  QString yesOrNo = " ";
-			  if (the_line.contains("OKQSY"))
-			  {
-				yesOrNo = QString(" OKQSY");
-				on_stopTxButton_clicked();
-			  }
-			  else
-			  {
-				yesOrNo = QString(" NOQSY");
-			  }
-				  
-			  QString qNewMessage = QString("$ ") + qDXCall + yesOrNo;
-			  m_QSYMessageWidget.reset (new QSYMessage(qNewMessage, qDXCall));
-			  
-			  //connect to signal finish
-			  connect (this, &MainWindow::finished, &QSYMessage::close);	
-			  QPoint mainPos = this->mapToGlobal(QPoint(0,0));
-			  m_QSYMessageWidget->show();
-			  m_QSYMessageWidget->move(mainPos.x() + this->width()/2 - (m_QSYMessageWidget->width())/2, mainPos.y() + this->height()/2 - (m_QSYMessageWidget->height())/2);
-			  m_QSYMessageWidget->raise();
-			  m_QSYMessageWidget->activateWindow();	
-		  }
-	  }	  
+  else if(the_line.contains(qDXCall + QString(" "))) {
+    if ((the_line.contains("OKQSY") || the_line.contains("NOQSY"))) {
+      QStringList bhList = the_line.split(" ",SkipEmptyParts);
+      qsizetype index = (bhList.indexOf(qDXCall));
+      if(index != (-1)) {
+        QString yesOrNo = " ";
+        if (the_line.contains("OKQSY")) {
+          yesOrNo = QString(" OKQSY");
+        } else {
+          yesOrNo = QString(" NOQSY");
+        }
+        on_stopTxButton_clicked();
+
+        QString qNewMessage = QString("$ ") + qDXCall + yesOrNo;
+        m_QSYMessageWidget.reset (new QSYMessage(qNewMessage, qDXCall));
+
+        //connect to signal finish
+        connect (this, &MainWindow::finished, &QSYMessage::close);
+        QPoint mainPos = this->mapToGlobal(QPoint(0,0));
+        m_QSYMessageWidget->show();
+        m_QSYMessageWidget->move(mainPos.x() + this->width()/2 - (m_QSYMessageWidget->width())/2, mainPos.y() + this->height()/2 - (m_QSYMessageWidget->height())/2);
+        m_QSYMessageWidget->raise();
+        m_QSYMessageWidget->activateWindow();
+      }
+    }
   }
 }
 
@@ -2840,6 +2819,13 @@ void MainWindow::statusChanged()
   }
   if (ui->tx1->text()=="" && !(m_mode=="FT8" && (SpecOp::HOUND==m_specOp or SpecOp::FOX==m_specOp))
       && !m_bDoubleClicked) ui->txb6->click();
+
+  if (m_config.enable_VHF_features()) {
+    ui->actionQSYMessage_Creator->setVisible(true);
+  } else {
+    ui->actionQSYMessage_Creator->setVisible(false);
+    if(m_QSYMessageCreatorWidget) m_QSYMessageCreatorWidget.reset();
+  }
 }
 
 bool MainWindow::eventFilter (QObject * object, QEvent * event)
@@ -3252,23 +3238,21 @@ void MainWindow::on_actionAstronomical_data_toggled (bool checked)
 
 void MainWindow::on_actionQSYMessage_Creator_triggered()
 {
-    if (!m_QSYMessageCreatorWidget)
-    {
-	  m_QSYMessageCreatorWidget.reset (new QSYMessageCreator);
-      // hook up termination signal
-      connect (this, &MainWindow::finished, &QSYMessageCreator::close);
-	  //connect to signal from QSYMessageCreator
-      connect (m_QSYMessageCreatorWidget.data (), &QSYMessageCreator::sendMessage, this, &MainWindow::update_tx5);	
-      connect (m_QSYMessageCreatorWidget.data (), &QSYMessageCreator::sendChkBoxChange, this, &MainWindow::update_QSYMessageCreatorCheckBoxStatus);	 
-      connect (m_QSYMessageCreatorWidget.data (), &QSYMessageCreator::sendQSYMessageCreatorStatus, this, &MainWindow::setQSYMessageCreatorStatus);	  
-	}	  
-	  m_QSYMessageCreatorValue = true;
-	  QPoint mainPos = this->mapToGlobal(QPoint(0,0));
-      m_QSYMessageCreatorWidget->showNormal();
-	  m_QSYMessageCreatorWidget->move(mainPos.x() + this->width() - (m_QSYMessageCreatorWidget->width()), mainPos.y()) ;
-	  m_QSYMessageCreatorWidget->raise();
-	  m_QSYMessageCreatorWidget->activateWindow();	
-    
+  if (!m_QSYMessageCreatorWidget) {
+    m_QSYMessageCreatorWidget.reset (new QSYMessageCreator);
+    // hook up termination signal
+    connect (this, &MainWindow::finished, &QSYMessageCreator::close);
+    //connect to signal from QSYMessageCreator
+    connect (m_QSYMessageCreatorWidget.data (), &QSYMessageCreator::sendMessage, this, &MainWindow::update_tx5);
+    connect (m_QSYMessageCreatorWidget.data (), &QSYMessageCreator::sendChkBoxChange, this, &MainWindow::update_QSYMessageCreatorCheckBoxStatus);
+    connect (m_QSYMessageCreatorWidget.data (), &QSYMessageCreator::sendQSYMessageCreatorStatus, this, &MainWindow::setQSYMessageCreatorStatus);
+  }
+  m_QSYMessageCreatorValue = true;
+  QPoint mainPos = this->mapToGlobal(QPoint(0,0));
+  m_QSYMessageCreatorWidget->showNormal();
+  m_QSYMessageCreatorWidget->move(mainPos.x() + this->width() - (m_QSYMessageCreatorWidget->width()), mainPos.y()) ;
+  m_QSYMessageCreatorWidget->raise();
+  m_QSYMessageCreatorWidget->activateWindow();
 }
 
 void MainWindow::on_fox_log_action_triggered()

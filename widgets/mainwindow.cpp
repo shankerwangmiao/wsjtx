f14a2632b0732e9c3048b8a8fe0cd047f31f5101
Joe Taylor <joe@princeton.edu>
2024-10-22 11:28:04 -0400

    QMAP and WSJT-X: single click in Active Stations window to select a station, double-click to also Enable Tx.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -4156,9 +4156,9 @@ void MainWindow::callSandP2(int n)
   static qint64 ms0=0;
   qint64 ms=QDateTime::currentMSecsSinceEpoch();
   if(ui->autoButton->isChecked()) {
-    if((ms-ms0)<=500) ui->autoButton->click(); // Disable Tx on double click
-  } else if((ms-ms0)>500) {
-    ui->autoButton->click(); // Enable Tx on single click
+    if((ms-ms0)>500) ui->autoButton->click(); // Enable Tx on double click
+  } else if((ms-ms0)<=500) {
+    ui->autoButton->click(); // Disable Tx on single click
   }
   ms0=ms;
   if(m_transmitting) m_restart=true;
